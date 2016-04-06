fe091615d17f71530907123f0a06c652f6aaeab1
Solmaz Salehian <ssalehian@oakland.edu>
2016-04-06 16:21:53 -0400

    Added files via upload

--- axpy/axpy_c++_async.cpp
+++ axpy/axpy_c++_async.cpp
@@ -1,22 +1,17 @@
 // example for thread::join
+#include <stdlib.h>
 #include <iostream>       // std::cout
 #include <chrono>         // std::chrono::seconds
-#include <future>
 #include <pthread.h>
-#include <thread> 
-#include <stdlib.h> 
+#include <thread>
 #include <sys/timeb.h>
+#include <math.h>
 #include <stdio.h>
-//#define REAL REAL
 #include <future>
 #include <vector>
+
 using namespace std;
 
-#define NUM_THREADS 1
-#define vector_length 102400
-
-#define BASE 1000
-#define REAL float
 /* read timer in second */
 double read_timer() {
     struct timeb tm;
@@ -30,6 +25,11 @@ double read_timer_ms() {
     ftime(&tm);
     return (double) tm.time * 1000.0 + (double) tm.millitm;
 }
+#define NUM_THREADS 64 
+#define vector_length 102400
+#define REAL float
+std::vector <std::future<void>> f(NUM_THREADS);
+
 
 void init(REAL *A, int N){
 int i;
@@ -46,64 +46,72 @@ double check(REAL A[], REAL B[], int N) {
         sum += A[i] - B[i];
     }
     return sum;
+int num_runs = 10;
 }
 
-//===========================async-future
+//===========================
+void axpy_cmth(int start,int end, REAL *Y, REAL *X, REAL a) {
+    int i;
+    for (i = start; i <end; ++i)
+        Y[i] += a * X[i];     
+     }
 
-void axpy_async_task(int N, REAL *Y, REAL *X, REAL a) {
-
- if (N<=BASE) {
-       int i;
-       for (i = 0; i < N; ++i)
-          Y[i] += a * X[i];
-            } else {
-std::future<void> f = std::async(std::launch::async,axpy_async_task,N/2, Y, X, a);
-        axpy_async_task(N-N/2, Y+N/2, X+N/2, a);
-       f.get();     
-                   }
-    }
+//void axpy_cmth(int N, REAL *Y, REAL *X, REAL a, int NUM_THREADS);
 
 //===========================          
 int main(int argc, char * argv[]) 
 {
-int N=vector_length;
-int M=NUM_THREADS;
-
+int  N=vector_length;
+int threadno = NUM_THREADS;
 double elapsed;
 double elapsed_dist;
 
 if (argc < 2) {
-  fprintf(stderr, "Usage: axpy <n> [<#threads(%d)>] (n should be dividable by #threads)\n", M);
+  fprintf(stderr, "Usage: axpy <n> [<#threads(%d)>] (n should be dividable by #threads)\n", threadno);
 exit(1);
  }
-  N = atoi(argv[1]);
-  if (argc > 2)  M=atoi(argv[2]);
+ N = atoi(argv[1]);
+ if (argc > 2) threadno = atoi(argv[2]);
 REAL a=0.1234;
 
-srand48((1<<12));
+//printf("%d: %d\n", N, NUM_THREADS);
+
+
+srand48((1 << 12));
 REAL *X =(REAL *) malloc(sizeof(REAL)*N);
 REAL *Y = (REAL *) malloc(sizeof(REAL)*N);
-REAL *Y_parallel = (REAL *) malloc(sizeof(REAL)*N);
+//REAL X[N];
+//REAL Y[N];
+    init(X, N);
+    init(Y, N);
 
-init(X,N);
-init(Y,N);
 
-int i, num_runs=10;
+
+//std::thread thread[ threadno ];
 elapsed=read_timer();
- for (i=0;i<num_runs; i++) axpy_async_task( N, Y, X, a);
-elapsed=(read_timer() - elapsed)/num_runs; 
+int i,j;
+for(i=0; i<threadno;++i)
+{       int tid = i;
+        int i_end,i_start, Nt;
+        Nt = N/threadno;
+        i_start = tid*Nt;
+        i_end = (tid+1)*Nt; 
+        //printf("creating thread: %d\n", i);
+        f[i]=std::async(std::launch::async,axpy_cmth,i_start,i_end,(REAL *)Y, (REAL *)X, a); 
+}      
+for(j=0;j<threadno;++j)
+ f[j].get();// join the first thread 
+
+elapsed=(read_timer() - elapsed); 
 
     printf("======================================================================================================\n");
-    printf("\tAXPY: Y[N] = Y[N] + a*X[N], N=%d, %d tasks for dist\n", N, M);
+  printf("\tAXPY: Y[N] = Y[N] + a*X[N], N=%d, %d tasks for dist\n", N, threadno);
     printf("------------------------------------------------------------------------------------------------------\n");
     printf("Performance:\t\tRuntime (ms)\t MFLOPS \t\tError (compared to base)\n");
     printf("------------------------------------------------------------------------------------------------------\n");
-    printf("axpy_c++_async:\t\t%4f\t%4f \t\t%g\n", elapsed * 1.0e3, (2.0 * N) / (1.0e6 * elapsed), check(Y, Y_parallel, N));
+    printf("axpy_base:\t\t%4f\t%4f \t\t%g\n", elapsed * 1.0e3, (2.0 * N) / (1.0e6 * elapsed), check(Y, Y, N));
   
 
-free(Y);
-free(Y_parallel);
-free(X);
 
 return 0;
 }
